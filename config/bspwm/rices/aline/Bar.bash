6513f7402e026ef8cc2ced25203304f50a8782d1
ilshatvm <ilshatvm13@gmail.com>
2025-05-26 05:42:46 +0500

    working version

--- config/bspwm/rices/aline/Bar.bash
+++ config/bspwm/rices/aline/Bar.bash
@@ -1,4 +1,31 @@
-# This file launch the bar/s
-for mon in $(polybar --list-monitors | cut -d":" -f1); do
-	MONITOR=$mon polybar -q aline-bar -c "${HOME}"/.config/bspwm/rices/"${RICE}"/config.ini &
-done
+TOP_PADDING="51"
+BOTTOM_PADDING="1"
+LEFT_PADDING="1"
+RIGHT_PADDING="1"
+
+color_file=${HOME}"/.config/bspwm/rices/"${RICE}"/bar-colors.ini"
+
+change_used_color() {
+	config_file=${HOME}"/.config/bspwm/rices/"${1}"/config.ini"
+	sed -i 's~include-file.*bar-colors.ini~include-file = '"$color_file"'~' "$config_file" 
+}
+change_current_bar() {
+	theme_config=${HOME}"/.config/bspwm/rices/"${RICE}"/theme-config.bash"
+	sed -i 's~CURRENT_BAR=.*~CURRENT_BAR="'$DEFAULT_BAR'"~' "$theme_config" 
+}
+
+if [ -n "$1" ] && [ "$1" == "current" ]
+then
+	change_used_color $CURRENT_BAR
+	# This file launch the bar/s
+	for mon in $(polybar --list-monitors | cut -d":" -f1); do
+		MONITOR=$mon polybar -q aline-bar -c "${HOME}"/.config/bspwm/rices/"${CURRENT_BAR}"/config.ini &
+	done
+else
+	change_used_color $DEFAULT_BAR
+	change_current_bar
+	# This file launch the bar/s	
+	for mon in $(polybar --list-monitors | cut -d":" -f1); do
+		MONITOR=$mon polybar -q aline-bar -c "${HOME}"/.config/bspwm/rices/"${DEFAULT_BAR}"/config.ini &
+	done
+fi
--- config/bspwm/rices/emilia/Bar.bash
+++ config/bspwm/rices/emilia/Bar.bash
@@ -1,4 +1,38 @@
-# This file launch the bar/s
-for mon in $(polybar --list-monitors | cut -d":" -f1); do
-	MONITOR=$mon polybar -q emi-bar -c "${HOME}"/.config/bspwm/rices/"${RICE}"/config.ini &
-done
+TOP_PADDING="50"
+BOTTOM_PADDING="1"
+LEFT_PADDING="1"
+RIGHT_PADDING="1"
+
+# path to the file with colors of the current theme
+color_file=${HOME}"/.config/bspwm/rices/"${RICE}"/bar-colors.ini"
+
+# change bar color scheme
+# when using you need to pass $CURRENT_BAR or $DEFAULT_BAR
+change_bar_colors() {
+	# path to the config file of the selected bar
+	config_file=${HOME}"/.config/bspwm/rices/"${1}"/config.ini"
+	# changes the color scheme for the selected bar in the file "config.ini"
+	sed -i 's~include-file.*bar-colors.ini~include-file = '"$color_file"'~' "$config_file" 
+}
+# changes the value of the string "CURRENT_BAR" to the default value
+change_current_bar() {
+	theme_config=${HOME}"/.config/bspwm/rices/"${RICE}"/theme-config.bash"
+	sed -i 's~CURRENT_BAR=.*~CURRENT_BAR="'$DEFAULT_BAR'"~' "$theme_config" 
+}
+
+
+if [ -n "$1" ] && [ "$1" == "current" ]
+then
+	change_bar_colors $CURRENT_BAR
+    # This file launch the bar/s
+	for mon in $(polybar --list-monitors | cut -d":" -f1); do
+		MONITOR=$mon polybar -q emi-bar -c "${HOME}"/.config/bspwm/rices/"${CURRENT_BAR}"/config.ini &
+	done
+else
+	change_bar_colors $DEFAULT_BAR
+	change_current_bar
+    # This file launch the bar/s
+	for mon in $(polybar --list-monitors | cut -d":" -f1); do
+		MONITOR=$mon polybar -q emi-bar -c "${HOME}"/.config/bspwm/rices/"${DEFAULT_BAR}"/config.ini &
+	done
+fi
